75c06cd2156af574694f9a9d9b0bc1e19180bf19
Joe Finney <josephfinney@LIVE.COM>
2026-04-08 23:38:14 -0500

    Add ParagraphDetection setting and toggle to settings UI
    Introduced a new ParagraphDetection user setting (default: True) to control paragraph wrapping detection. Updated App.config, Settings.settings, and Settings.Designer.cs to support this setting. Added a ToggleSwitch to GeneralSettings.xaml for user control, with event handlers in GeneralSettings.xaml.cs to persist changes. Also updated the code generation version in Settings.Designer.cs.

--- Text-Grab/Properties/Settings.Designer.cs
+++ Text-Grab/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Text_Grab.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "18.4.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "18.5.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -956,6 +956,18 @@ namespace Text_Grab.Properties {
             }
             set {
                 this["RegisterOpenWith"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool ParagraphDetection {
+            get {
+                return ((bool)(this["ParagraphDetection"]));
+            }
+            set {
+                this["ParagraphDetection"] = value;
             }
         }
     }
